e85d1773ed188e8a5e1904503305b9411a13a733
Simon Busch <morphis@gravedo.de>
2012-08-24 06:03:02 +0200

    Change some settings related to OE and jb support

--- samsung-ipc/device/maguro/maguro_loader.h
+++ samsung-ipc/device/maguro/maguro_loader.h
@@ -27,10 +27,10 @@
 #ifndef __MAGURO_LOADER_H__
 #define __MAGURO_LOADER_H__
 
-#define I9250_RADIO_IMAGE "/dev/block/platform/omap/omap_hsmmc.0/by-name/radio"
+#define I9250_RADIO_IMAGE "/dev/disk/by-partlabel/radio"
 #define I9250_SECOND_BOOT_DEV "/dev/umts_boot1"
 
-#define I9250_BOOT_LAST_MARKER 0x0030ffff
+#define I9250_BOOT_LAST_MARKER 0x00b0ffff
 #define I9250_BOOT_REPLY_MAX 20
 
 #define I9250_GENERAL_ACK "\x02\x00\x00\x00"
